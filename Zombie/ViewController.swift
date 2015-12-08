1fdbb506b9416adfe6fbe8e16208433679471c7e
Pierre Kopa <pierre.kopaczewski@blablacar.com>
2015-12-08 20:55:54 +0100

    Initial Commit

---
+++ Zombie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Zombie
+//
+//  Created by Pierre Kopaczewski on 08/12/2015.
+//  Copyright © 2015 Pierre Kopaczewski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
